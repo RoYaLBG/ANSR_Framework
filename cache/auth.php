8937a31d3a524c10d28a22a8b9c24c017b59a874
RoYaLBG <royal4o@gmail.com>
2017-10-18 22:08:13 +0300

    deleting chache

--- cache/auth.php
+++ cache/auth.php
@@ -1,9 +1,2 @@
 <?php return array (
-  'DefaultApp\\Controller\\UsersController' => 
-  array (
-    'profile' => 
-    array (
-      0 => 'USER',
-    ),
-  ),
 );
--- cache/interceptors.php
+++ cache/interceptors.php
@@ -1,10 +1,2 @@
 <?php return array (
-  'DefaultApp\\Controller\\HomeController' => 
-  array (
-    'intercepted' => 
-    array (
-      0 => 'DefaultApp\\Interceptor\\LoginRequiredInterceptor',
-      1 => 'DefaultApp\\Interceptor\\RoleRequiredInterceptor',
-    ),
-  ),
 );
